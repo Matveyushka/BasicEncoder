54748b4174d97c51369d8e95e5f41da3ae02d6dd
Matveyushka <waytokey@mail.ru>
2021-05-19 15:37:08 +0300

    Fixed some algorithms.

--- app/src/main/java/com/example/basicencoder/cipher/ICipher.kt
+++ app/src/main/java/com/example/basicencoder/cipher/ICipher.kt
@@ -3,7 +3,7 @@ package com.example.basicencoder.cipher
 import com.example.basicencoder.utils.Alphabet
 
 interface ICipher {
-    val alphabets: Alphabet
+    val alphabet: Alphabet
     fun encode(source: String, arguments: Map<String, Any>): String
     fun decode(source: String, arguments: Map<String, Any>): String
 }
